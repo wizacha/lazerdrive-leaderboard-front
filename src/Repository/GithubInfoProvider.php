6fc3030600538641e131f1a45dc696586e43231d
Matthieu Napoli <matthieu@mnapoli.fr>
2015-12-18 13:15:21 +0100

    Cache GitHub info

--- src/Repository/GithubInfoProvider.php
+++ src/Repository/GithubInfoProvider.php
@@ -12,6 +12,8 @@ use Leaderboard\Player;
 class GithubInfoProvider implements PlayerRepository
 {
     const API_URL = 'https://api.github.com/users/';
+    const CACHE_KEY_PREFIX = 'GithubInfoProvider.user.';
+    const SECONDS_IN_DAY = 3600*24;
 
     /**
      * @var PlayerRepository
@@ -34,7 +36,7 @@ class GithubInfoProvider implements PlayerRepository
         $players = $this->decoratedRepository->getTopPlayers();
 
         return array_map(function (Player $player) {
-            $githubInfo = $this->getGithubInfo($player->getName());
+            $githubInfo = $this->getCachedGithubInfo($player->getName());
 
             if (!empty($githubInfo)) {
                 $player->setCompany((string) $githubInfo['company']);
@@ -46,11 +48,33 @@ class GithubInfoProvider implements PlayerRepository
     }
 
     /**
+     * Fetch GitHub data from the cache, or from GitHub if not in cache.
+     *
+     * If no data was found, an empty array is returned.
+     */
+    private function getCachedGithubInfo(string $username) : array
+    {
+        if (! function_exists('apc_exists')) {
+            return $this->getGithubInfo($username);
+        }
+
+        $key = self::CACHE_KEY_PREFIX . $username;
+
+        if (apc_exists($key)) {
+            return apc_fetch($key);
+        }
+
+        $data = $this->getGithubInfo($username);
+
+        apc_store($key, $data, self::SECONDS_IN_DAY);
+
+        return $data;
+    }
+
+    /**
      * Fetch GitHub data for the given user name.
      *
      * If no data was found, an empty array is returned.
-     *
-     * @return array
      */
     private function getGithubInfo(string $username) : array
     {
