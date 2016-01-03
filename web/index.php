36721bb27b759eb4b55417d84ea0dc53a63103f5
Matthieu Napoli <matthieu@mnapoli.fr>
2016-01-03 13:54:27 +0100

    Refactor the application with Slim, PHP-DI, Puli and Silly

--- web/index.php
+++ web/index.php
@@ -1,15 +1,14 @@
 <?php
 
-use Interop\Container\ContainerInterface;
 use Leaderboard\Application\Deployer;
+use Leaderboard\Application\Kernel;
 use Leaderboard\Repository\PlayerRepository;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\TextResponse;
 
-/** @var ContainerInterface $container */
-$container = require __DIR__ . '/../res/bootstrap.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App($container);
+$app = new \Slim\App((new Kernel)->createContainer());
 
 $app->get('/', function (PlayerRepository $repository, Twig_Environment $twig) {
     return new HtmlResponse($twig->render('/app/views/index.twig', [
