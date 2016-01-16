0b76f0d0c7adb5c988c780c403cbd8dd3cb9829d
Vincent Dechenaux <vincent.dechenaux@gmail.com>
2016-01-16 08:21:54 +0100

    Use the "color" in DB

--- src/Player.php
+++ src/Player.php
@@ -25,18 +25,24 @@ class Player
     private $company;
 
     /**
+     * @var string
+     */
+    private $color;
+
+    /**
      * By default it's the default Gravatar.
      *
      * @var string
      */
     private $avatarUrl = 'http://www.gravatar.com/avatar/?d=identicon';
 
-    public function __construct(string $name, int $highScore, bool $isOnline, string $company = '')
+    public function __construct(string $name, int $highScore, bool $isOnline, string $company = '', string $color = '')
     {
         $this->name = $name;
         $this->highScore = $highScore;
         $this->isOnline = $isOnline;
         $this->company = $company;
+        $this->color = $color;
     }
 
     public function getName() : string
@@ -72,5 +78,15 @@ class Player
     public function getAvatarUrl() : string
     {
         return $this->avatarUrl;
+    }
+
+    public function getColor() : string
+    {
+        return $this->color;
+    }
+
+    public function setColor($color) : string
+    {
+        $this->color = $color;
     }
 }
